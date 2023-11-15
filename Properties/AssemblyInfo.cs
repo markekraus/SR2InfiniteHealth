ecc5a667eaaad82c50fa7b8383896877884ba6bc
Mark Kraus <github@markekraus.com>
2023-11-15 15:03:30 -0800

    Updated for SR2 v0.3.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Runtime.InteropServices;
 using InfiniteHealth;
 using MelonLoader;
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
 [assembly: MelonAuthorColor(255, 255, 0, 188)]
 [assembly: MelonColor(255, 229, 85, 112)]
-[assembly: MelonInfo(typeof(Entry), "Infinite health", "1.0.0", "MarkEKraus", null)]
+[assembly: MelonInfo(typeof(Entry), "Infinite health", "2.0.0", "MarkEKraus", null)]
 [assembly: MelonGame("MonomiPark", "SlimeRancher2")]
 [assembly: AssemblyTitle("InfiniteHealth")]
 [assembly: AssemblyDescription("Slime Rancher 2 mod that enables infinite health.")]
@@ -18,5 +18,5 @@ using MelonLoader;
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9d03a24c-6524-475d-98c6-6428d90d5223")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 
